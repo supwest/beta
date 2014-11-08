5860a55301a859b1103ce9b10729c487a77f78e7
unknown <cullywest@gmail.com>
2014-11-08 17:55:43 -0600

    Enemies essplode

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour {
+
+	public GameObject gib;
+
+	void OnTriggerEnter(){
+
+		Instantiate (gib, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+
+	}
+
+}
